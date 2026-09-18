99751911627036df00d595d442464db3d906893f
gaurav.painuly <gaurav.painuly@servicenow.com>
2026-09-18 18:35:19 +0530

    shapes with random color

--- dom_projects/image_generator/script.js
+++ dom_projects/image_generator/script.js
@@ -14,28 +14,32 @@ const shapeTypeInput = document.querySelector("#shapeType");
 const shapeCountInput = document.querySelector("#shapeCount");
 const shapeColorInput = document.querySelector("#shapeColor");
 
-// Using data-* attributes to get stable JavaScript hooks, so we can select buttons by their behavior
-// Styles on class or ids may change in future however using data-* we are being more intentional towards it's behavior
+/**
+ * Buttons
+ *
+ * Using data-* attributes to get stable JavaScript hooks, so we can select buttons by their behavior
+ * Styles on class or ids may change in future however using data-* we are being more intentional towards it's behavior
+ */
+
 const generateButton = document.querySelector('[data-action="generate"]');
 const clearShapeButton = document.querySelector('[data-action="clear-shapes"');
+const generateRanColorButton = document.querySelector(
+  '[data-action="generate-with-random-color"]',
+);
 
 const shapeContainer = document.querySelector(".shape-container");
 
-generateButton.addEventListener("click", () => {
-  if (!shapeTypeInput.checkValidity()) {
-    shapeTypeInput.reportValidity();
-    return;
-  }
+function validateInputs(...inputElements) {
+  return inputElements.every((inputElement) => {
+    if (!inputElement.checkValidity()) {
+      inputElement.reportValidity();
+      return false;
+    }
+    return true;
+  });
+}
 
-  if (!shapeCountInput.checkValidity()) {
-    shapeCountInput.reportValidity();
-    return;
-  }
-
-  const shapeType = shapeTypeInput.value;
-  const shapeCount = Number(shapeCountInput.value);
-  const shapeColor = shapeColorInput.value;
-
+function drawShapes(shapeContainer, shapeType, shapeCount, shapeColor) {
   // Auto clear before adding any more children
   shapeContainer.replaceChildren();
 
@@ -45,9 +49,42 @@ generateButton.addEventListener("click", () => {
     shape.style.backgroundColor = shapeColor;
     shapeContainer.appendChild(shape);
   }
+}
+
+generateButton.addEventListener("click", () => {
+  if (!validateInputs(shapeTypeInput, shapeCountInput)) return;
+
+  drawShapes(
+    shapeContainer,
+    shapeTypeInput.value,
+    Number(shapeCountInput.value),
+    shapeColorInput.value,
+  );
 });
 
 // Clear children on demand
 clearShapeButton.addEventListener("click", () => {
   shapeContainer.replaceChildren();
 });
+
+generateRanColorButton.addEventListener("click", () => {
+  const randomColorCode = randomHexCodeGenerator();
+  drawShapes(
+    shapeContainer,
+    shapeTypeInput.value,
+    Number(shapeCountInput.value),
+    randomColorCode,
+  );
+});
+
+function randomHexCodeGenerator() {
+  let color = "#";
+  const HEX_COLORS = "0123456789ABCDEF";
+  const HEX_CODE_LENGTH = 6;
+
+  for (let i = 0; i < HEX_CODE_LENGTH; i++) {
+    let randomCharIndex = Math.floor(Math.random() * HEX_COLORS.length);
+    color += HEX_COLORS[randomCharIndex];
+  }
+  return color;
+}
